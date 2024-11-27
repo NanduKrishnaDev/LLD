91ddc8d08a65cf56957bbcfcdabcbfea526e8fe2
nanduKrishnaQB <nandu.krishna@qburst.com>
2024-11-27 11:06:40 +0530

    implementations

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, createContext, lazy, useState } from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
+import InfiniteScrollB from "./components/InfiniteScrollB";
+// import Comments from "./components/Comments";
+
+const Comments = lazy(() => import("./components/Comments"));
+const LiveChat = lazy(() => import("./components/LiveChat"));
+const Search = lazy(() => import("./components/Search"));
+
+export const ThemeContext = createContext();
 
 function App() {
+  const [theme, setTheme] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense>
+      <ThemeContext.Provider value={[theme, setTheme]}>
+        <div className={`app-container ${!theme && "bg-darker"} min-h-[100vh]`}>
+          <Header />
+          <div className="px-8">
+            <Outlet />
+          </div>
+        </div>
+      </ThemeContext.Provider>
+    </Suspense>
   );
 }
 
-export default App;
+const appRouter = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <InfiniteScrollB />,
+      },
+      {
+        path: "/comments",
+        element: <Comments />,
+      },
+      {
+        path: "/live",
+        element: <LiveChat />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+    ],
+  },
+]);
+
+const AppProvider = () => <RouterProvider router={appRouter} />;
+
+export default AppProvider;
